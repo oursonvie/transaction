1c2645f743ec6d436e0be9e9406c4d037f99ac64
Tonny <oursonvie@qq.com>
2018-06-06 09:11:29 +0800

    allowed switch for single dcenter

--- client/admin/lcenter/district/eachDLCenter.js
+++ client/admin/lcenter/district/eachDLCenter.js
@@ -1,5 +1,18 @@
+Template.eachDLCenter.helpers({
+  allowAccess: function() {
+    return this.allowAccess
+  }
+})
+
 Template.eachDLCenter.events({
-  "click li": function() {
+  "click .btn-outline-secondary": function() {
     Session.set('action', { type:'view',  id: this._id} )
+  },
+  "click .fa-power-off": function() {
+    PromiseMeteorCall('changeAccess', this._id)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
   }
 });
--- server/methods/CURD.js
+++ server/methods/CURD.js
@@ -5,5 +5,26 @@ Meteor.methods({
     } else {
       throw new Meteor.Error( '500', 'No Premission' );
     }
+  },
+  changeAccess: function(id) {
+    if (this.userId) {
+      // target dcenter
+
+      let target = DLearningCenter.findOne({_id:id})
+
+      result = DLearningCenter.update(
+        {_id:id},
+        {$set: {
+          allowAccess: !target.allowAccess
+        }}
+      )
+
+      message = {action:'update', target: id, result: result}
+
+      return message
+
+    } else {
+      throw new Meteor.Error( '500', 'No Premission' );
+    }
   }
 });
